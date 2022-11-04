99e2039fdb420f1def56731a716f86f104019f1a
GOD-oda <last.pass.oda@gmail.com>
2022-11-05 01:17:33 +0900

    Feat: Install friendsofphp/php-cs-fixer

---
+++ src/.php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database/factories',
+        __DIR__ . '/database/seeders',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setFinder($finder);
