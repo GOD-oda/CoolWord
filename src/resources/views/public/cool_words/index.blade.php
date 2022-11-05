ba247efc69db1c0809f019fd4128db2a067f8305
GOD-oda <last.pass.oda@gmail.com>
2022-11-05 19:06:20 +0900

    fix: index ui in public

--- src/resources/views/public/cool_words/index.blade.php
+++ src/resources/views/public/cool_words/index.blade.php
@@ -5,42 +5,45 @@
     <div class="container py-3">
       <form action="{{ route('cool_words.index') }}">
         <div class="row">
-          <div class="col-6">
+          <div class="col-5">
             @include('public.cool_words.form_components.name', ['value' => $input['name'] ?? ''])
           </div>
-        </div>
 
-        <div class="row">
-          <div class="col-12">
+          <div class="col-5">
             @include('public.cool_words.form_components.tag', ['tags' => $tags, 'originalTagIds' => $originalTagIds])
           </div>
-        </div>
 
-        <div class="row">
-          <div class="col-4 mt-auto">
+          <div class="col-2">
             <button type="submit" class="btn btn-primary">検索</button>
           </div>
         </div>
       </form>
 
-      <div class="row">
+      <table class="table">
+        <thead>
+        <tr>
+          <th scope="col" colspan="5">Name</th>
+          <th scope="col" colspan="5">Tag</th>
+          <th scope="col" colspan="2">閲覧数</th>
+        </tr>
+        </thead>
+
+        <tbody>
         @foreach ($paginator->items() as $coolWord)
-          <div class="col-sm-12 col-md-4 card-box">
-            <h4><a href="{{ route('cool_words.show', ['id' => $coolWord['id']]) }}">{{ $coolWord['name'] }}</a></h4>
-            <div class="d-flex">
-              <div class="tags d-flex justify-content-between p-1">
-                @foreach ($coolWord['tags'] as $tag)
-                  <span class="mx-1">{{ $tag['name'] }}</span>
-                @endforeach
-              </div>
-              <div class="d-flex align-items-center">
-                <span class="material-symbols-outlined">visibility</span>
-                <span class="ms-2">{{ $coolWord['views'] }}</span>
-              </div>
-            </div>
-          </div>
+          <tr class="cool-word">
+            <th scope="row" colspan="5">
+              <a href="{{ route('cool_words.show', ['id' => $coolWord['id']]) }}">{{ $coolWord['name'] }}</a>
+            </th>
+            <td colspan="5">
+              @foreach ($coolWord['tags'] as $tag)
+                <span class="mx-1 tag">{{ $tag['name'] }}</span>
+              @endforeach
+            </td>
+            <td colspan="2">{{ $coolWord['views'] }}</td>
+          </tr>
         @endforeach
-      </div>
+        </tbody>
+      </table>
 
       <div class="row my-3">
         {{ $paginator->links() }}
